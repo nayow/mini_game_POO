e5a16034d7cea9855ec76a76d950110cde4c56a2
nayow <nayow@hotmail.fr>
2020-01-28 21:36:51 +0100

    modified version 3

--- lib/player3.rb
+++ lib/player3.rb
@@ -1,5 +1,4 @@
 class Player
-
     attr_accessor :name, :life_points
 
     def initialize(name)
@@ -13,7 +12,7 @@ class Player
 
     def gets_damage(damage_number)
         self.life_points -= damage_number
-        if self.life_points <= 0 then puts "Le joueur #{self.name} est mort!" end
+        if self.life_points <= 0 then puts "Le joueur #{self.name} est mort!","" end
     end
 
     def attacks(player_number)
@@ -26,11 +25,9 @@ class Player
     def compute_damage
         return rand(1..6)
     end
-
 end
 
 class HumanPlayer < Player
-
     attr_accessor :weapon_level
 
     def initialize(name)
@@ -51,23 +48,22 @@ class HumanPlayer < Player
         weapon_found = rand(1..6)
         puts "Tu as trouvé une arme de niveau #{weapon_found}"
         if weapon_found > @weapon_level
-            puts "Elle est meilleure, tu la prends!"
+            puts "Elle est meilleure, tu la prends!",""
             @weapon_level = weapon_found
-        else puts "Elle n'est pas vraiment mieux..." 
+        else puts "Elle n'est pas vraiment mieux..." ,""
         end
     end
 
     def search_health_pack
         health_pack_found = rand(1..6)
         if health_pack_found == 1
-            puts "Tu n'as rien trouvé..."
+            puts "Tu n'as rien trouvé...",""
         elsif health_pack_found >= 2 && health_pack_found <= 5
-            puts "Tu as trouvé 50 points de vie!"
+            puts "Tu as trouvé 50 points de vie!",""
             @life_points + 50 > 100 ? @life_points = 100 : @life_points += 50
         else 
-            puts "Tu as trouvé 80 points de vie!"
+            puts "Tu as trouvé 80 points de vie!",""
             @life_points + 80 > 100 ? @life_points = 100 : @life_points += 80
         end
     end
-
 end
